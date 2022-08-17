b73ec73eb6a36696825f9329e0bb88393c7ac82f
Raza Balbale <ribalbale@gmail.com>
2022-08-17 18:50:24 -0500

    Added script to create liquid template.

---
+++ scripts/add_liquid_template.sh
@@ -0,0 +1,13 @@
+TEMP_FOLDER=projects/$1/templates
+mkdir -p $TEMP_FOLDER
+TEMP_LIQUID_PATH=$TEMP_FOLDER/sample.liquid
+
+# Add Liquid Template
+touch $TEMP_LIQUID_PATH
+
+echo 'Single Item: {{ data.name }}
+Item List:
+{% for item in data.items %}
+    - {{ item.name }}
+{% endfor %}
+' >> $TEMP_LIQUID_PATH
